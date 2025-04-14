1fbc7552aa71f447d9dc55d5a5969161646a0e19
llauderesv <llauderesv@gmail.com>
2025-04-14 09:36:06 +0800

    add comments

--- check-urls-generator/checkUrls.js
+++ check-urls-generator/checkUrls.js
@@ -1,5 +1,10 @@
 import superagent from 'superagent';
 
+// This example is an iterator that checks the status of a list of URLs.
+// It uses the async iterator protocol to yield results as they are available.
+// It uses the superagent library to make HTTP requests.
+// The constructor takes an array of URLs.
+// The async iterator will check each URL and yield the result.
 export class CheckUrls {
   constructor(urls) {
     this.urls = urls;
